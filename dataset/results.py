d7a7de7b923e4f8e9718284bed6d483515978000
fionalin0221 <fionalin0221@gmail.com>
2025-07-22 11:22:07 +0800

    regular adjust

--- dataset/results.py
+++ dataset/results.py
@@ -5,7 +5,7 @@ import pandas as pd
 # base_path = "/workspace/Data/Results/Mix_NDPI/10WTC_Result/LP_6400"
 # output_file = "/workspace/Data/Results/Mix_NDPI/10WTC_Result/LP_6400/trial_3_cc_tani_results.csv"
 base_path = "/home/ipmclab-2/project/Results/Mix_NDPI/100WTC_Result/LP_3200"
-output_file = "/home/ipmclab-2/project/Results/Mix_NDPI/100WTC_Result/LP_3200/100WTC_LP3200_trial_1_hcc_tati_results.csv"
+output_file = "/home/ipmclab-2/project/Results/Mix_NDPI/100WTC_Result/LP_3200/100WTC_LP3200_trial_1_hcc_tati_results_new.csv"
 cl = "H"
 
 # Define trials and WSIs
@@ -17,7 +17,7 @@ num_trials = list(range(1, 2))  # 11 to 19
 # wsi_list = [6, 11, 39, 52, 144]
 # wsi_list = [2, 21, 50, 69, 81]
 # wsi_list = [1, 3, 6, 7, 8, 11, 12, 13, 14, 15, 39, 40, 42, 43, 52, 53, 54, 55, 67, 70, 71, 72, 88, 91, 95, 100, 108, 109, 111, 118, 122, 124, 130, 131, 135, 136, 137, 138, 143, 144, 145, 167, 168, 169, 170, 171, 173, 174, 175, 178, 179, 180, 183, 184, 185, 189, 191, 192, 202, 204, 206, 207, 208, 215, 217, 222, 223, 224, 225, 226, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 263, 264, 265, 266, 269, 275, 276, 277, 291, 296, 297, 298, 299, 300, 325, 328, 329, 330, 374, 375]
-wsi_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 48, 50, 51, 52, 54, 55, 56, 58, 59, 60, 62, 63, 64, 66, 67, 68, 70, 71, 73, 74, 75, 77, 78, 79, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 97, 99, 104, 107, 118, 120, 121, 122, 127, 129, 130, 135, 136, 138, 140, 141, 142, 144]
+wsi_list = [1, 2, 3, 4, 5] #, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 48, 50, 51, 52, 54, 55, 56, 58, 59, 60, 62, 63, 64, 66, 67, 68, 70, 71, 73, 74, 75, 77, 78, 79, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 97, 99, 104, 107, 118, 120, 121, 122, 127, 129, 130, 135, 136, 138, 140, 141, 142, 144]
 # wsi_list = [45, 49, 53, 57, 61, 65, 69, 72, 76, 80]
 # wsi_list = [2, 41, 69, 90, 110, 123, 134, 177, 190, 201]
 
@@ -31,7 +31,7 @@ for num_trial in num_trials:
             wsi = _wsi
         else:
             wsi = f"1{_wsi:04d}"
-        file_path = f"{base_path}/trial_{num_trial}/{wsi}/Metric/{wsi}_100WTC_LP3200_3_class_trial_{num_trial}_test_result.csv"
+        file_path = f"{base_path}/trial_{num_trial}/{wsi}/Metric/{wsi}_100WTC_LP3200_3_class_trial_{num_trial}_test_result_new.csv"
 
         if not os.path.exists(file_path):
             print(f"File not found: {file_path}")
--- trainer/run.py
+++ trainer/run.py
@@ -18,14 +18,14 @@ def main():
     worker = Worker(config)
     # worker.train()
     # worker.train_multi_model()
-    # worker.train_generation()
     # worker.test()
     # worker.contour_analysis_multi(0, )
     for wsi in wsis:
-        worker.train_one_WSI(wsi)
-        worker.test_one_WSI(wsi)
+        # worker.train_one_WSI(wsi)
+        # worker.test_one_WSI(wsi)
         # worker.test_TATI(wsi, 0, save_path = None, test_type = test_type)
         # worker.plot_TI_Result(wsi, 0, save_path = None)
+        worker.train_generation(wsi)
         # worker.contour_analysis(wsi, 0, save_path = None)
 
 
