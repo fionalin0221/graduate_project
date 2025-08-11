d7913fc944c4f4d340e6823c6837dcee7194ef8d
fionalin0221 <fionalin0221@gmail.com>
2025-08-11 09:42:55 +0800

    update for all class recall

--- dataset/count_correct_label.py
+++ dataset/count_correct_label.py
@@ -8,18 +8,21 @@ gt_dict = dict(zip(gt_data['file_name'], gt_data['label']))
 
 base_path = "/home/ipmclab/project/Results/Mix_NDPI/Generation_Training/40WTC_LP_6400/10090"
 
+classes = ["N", "C"]  # fixed classes
 results = []
 
-for trial in range(1, 4):
+for trial in range(1, 7):
     for gen in range(1, 5):
         # 10090_Gen{gen}_ND_zscore_ideal_patches_by_Gen{gen-1}
         # Gen{gen}_ND_zscore_ideal_patches_by_Gen{gen-1}_train
-        pred_path = f"{base_path}/trial_{trial}/Data/10090_Gen{gen}_ND_zscore_ideal_patches_by_Gen{gen-1}.csv"
+        pred_path = f"{base_path}/trial_{trial}/Data/Gen{gen}_ND_zscore_ideal_patches_by_Gen{gen-1}_train.csv"
         train_data = pd.read_csv(pred_path)
         
         correct = 0
         incorrect = 0
         total = 0
+
+        class_stats = {cls: {"Correct": 0, "Incorrect": 0, "Total": 0} for cls in classes}
 
         file_names = train_data['file_name'].tolist()
         pseudo_labels = train_data['label'].tolist()
@@ -32,9 +35,12 @@ for trial in range(1, 4):
             gt_label = gt_dict[fname]
             if pseudo_label == gt_label:
                 correct += 1
+                class_stats[gt_label]["Correct"] += 1
             else:
                 incorrect += 1
+                class_stats[gt_label]["Incorrect"] += 1
             total += 1
+            class_stats[gt_label]["Total"] += 1
 
         accuracy = 100 * correct / total if total > 0 else 0.0
 
@@ -44,15 +50,23 @@ for trial in range(1, 4):
         print(f"  Incorrect pseudo labels: {incorrect}")
         print(f"  Accuracy: {accuracy:2f}%")
 
-        results.append({
+        row = {
             "Trial": trial,
             "Gen": gen,
             "Total": total,
             "Correct": correct,
             "Incorrect": incorrect,
-            "Accuracy (%)": accuracy
-        })
+        }
+
+        # add per-class stats into the same row
+        for cls, stats in sorted(class_stats.items(), key=lambda x: x[0]):
+            row[f"Class_{cls}_Total"] = stats["Total"]
+            row[f"Class_{cls}_Correct"] = stats["Correct"]
+            row[f"Class_{cls}_Incorrect"] = stats["Incorrect"]
+            print(cls, stats["Total"],stats["Correct"], stats["Incorrect"])
+
+        results.append(row)
 
 df = pd.DataFrame(results)
-output_csv = os.path.join(base_path, "10090_all_pseudo_label_summary.csv")
+output_csv = os.path.join(base_path, "10090_train_pseudo_label_summary.csv")
 df.to_csv(output_csv, index=False)
