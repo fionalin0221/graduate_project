c6c69055ae38485f03e614a3a1e653346b5e1658
fionalin0221 <fionalin0221@gmail.com>
2026-03-08 11:43:19 +0800

    update results collection code

--- dataset/results_gen.py
+++ dataset/results_gen.py
@@ -3,42 +3,69 @@ import pandas as pd
 
 # Define paths
 result_type = "Mix"
-num_wsi = 20
-data_num = "ALL"
-num_trial = 2
-gen = 1
-num_class = 3
+base_model_num_wsi = 100
+base_model_data_num = 3200
+base_model_trial = 1
 
-# base_path = f"/workspace/Data/Results/{result_type}_NDPI/Generation_Training/{num_wsi}WTC_LP_{data_num}"
-# base_path = f"/home/ipmclab-2/project/Results/{result_type}_NDPI/Generation_Training/{num_wsi}WTC_LP_{data_num}"
-base_path = f"/home/ipmclab/project/Results/{result_type}_NDPI/Generation_Training/{num_wsi}WTC_LP_{data_num}"
-output_file = f"{base_path}/{num_wsi}WTC_LP{data_num}_trial_{num_trial}_generation_results.csv"
+num_wsi = 30
+data_num = 'ALL'
+num_trial = 2
+gen = 2
+num_class = 3
+error_train = False
+
+# base_dir = f"/workspace/Data/Results/{result_type}_NDPI/Generation_Training/{base_model_num_wsi}WTC_LP_{base_model_data_num}_trial_{base_model_trial}_based"
+# base_dir = f"/home/ipmclab-2/project/Results/{result_type}_NDPI/Generation_Training/{base_model_num_wsi}WTC_LP_{base_model_data_num}_trial_{base_model_trial}_based"
+base_dir = f"/home/ipmclab/project/Results/{result_type}_NDPI/Generation_Training/{base_model_num_wsi}WTC_LP_{base_model_data_num}_trial_{base_model_trial}_based"
+if num_wsi == 1:
+    base_path = f'{base_dir}/LP_{data_num}/{_wsi}/trial_{num_trial}'
+else:
+    base_path = f'{base_dir}/{num_wsi}WTC_LP_{data_num}/trial_{num_trial}'
+
+output_file = f"{base_path}/{num_wsi}WTC_LP{data_num}_trial_{num_trial}_tani_generation_results.csv"
 
 # Define trials and WSIs
-# HCC_wsi_list = [195, 275, 298, 314]
-HCC_wsi_list = []
-# CC_wsi_list = [1, 11, 55, 100, 131, 2, 41, 69, 90, 110]
+# HCC_wsi_list = []
+# CC_wsi_list = []
+
+# HCC 10WTC
+# HCC_wsi_list = [1, 12, 22, 33, 45, 56, 67, 76, 89, 91]
+# HCC_wsi_list = [6, 11, 39, 52, 144]
+
+# CC 10WTC
+# CC_wsi_list = [72, 108, 111, 116, 122, 124, 130, 131, 137, 138]
+# CC_wsi_list = [2, 21, 50, 69, 81]
+
+
+# HCC 40WTC
+# HCC_wsi_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 37, 38, 39, 41, 42, 43, 44]
 
 # Old CC 40WTC
 # CC_wsi_list =  [6, 8, 11, 12, 13, 14, 39, 52, 54, 55, 72, 108, 111, 116, 122, 124, 130, 131, 137, 138, 143, 144, 169, 170, 171, 175, 178, 180, 183, 184 , 185, 208, 244, 246, 247, 248, 291, 374, 375, 377]
 # CC_wsi_list = [3, 41, 71, 91, 123, 135,177, 192, 207, 222]
-# New CC 40WTC
 # CC_wsi_list = [1, 3, 6, 7, 8, 11, 12, 13, 14, 15, 39, 40, 42, 43, 52, 53, 54, 55, 67, 70, 71, 72, 88, 91, 95, 100, 108, 109, 111, 118, 122, 124, 130, 131, 135, 136, 175, 178, 191, 202]
+
+# New CC 40WTC
+# CC_wsi_list = [1, 6, 7, 8, 11, 12, 13, 14, 39, 40, 52, 53, 54, 55, 70, 72, 100, 108, 111, 118, 122, 124, 130, 131, 136, 137, 138, 143, 144, 169, 170, 171, 175, 178, 180, 183, 184, 191, 202]
 # CC_wsi_list = [2, 41, 69, 90, 110, 123, 134, 177, 190, 201]
 
-# HCC 10WTC
-# HCC_wsi_list = [1, 12, 22, 33, 45, 56, 67, 76, 89, 91]
-# HCC_wsi_list = [72, 108, 111, 116, 122, 124, 130, 131, 137, 138]
-# HCC_wsi_list = [6, 11, 39, 52, 144]
-# HCC_wsi_list = [2, 21, 50, 69, 81]
 
-# Mix 100WTC
+# HCC 100WTC
 # HCC_wsi_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 48, 50, 51, 52, 54, 55, 56, 58, 59, 60, 62, 63, 64, 66, 67, 68, 70, 71, 73, 74, 75, 77, 78, 79, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 97, 99, 104, 107, 118, 120, 121, 122, 127, 129, 130, 135, 136, 138, 140, 141, 142, 144]
-# CC_wsi_list = [1, 3, 6, 7, 8, 11, 12, 13, 14, 15, 39, 40, 42, 43, 52, 53, 54, 55, 67, 70, 71, 72, 88, 91, 95, 100, 108, 109, 111, 118, 122, 124, 130, 131, 135, 136, 137, 138, 143, 144, 145, 167, 168, 169, 170, 171, 173, 174, 175, 178, 179, 180, 183, 184, 185, 189, 191, 192, 202, 204, 206, 207, 208, 215, 217, 222, 223, 224, 225, 226, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 263, 264, 265, 266, 269, 275, 276, 277, 291, 296, 297, 298, 299, 300, 325, 328, 329, 330, 374, 375]
 # HCC_wsi_list = [45, 49, 53, 57, 61, 65, 69, 72, 76, 80]
+
+# CC 100WTC
+# CC_wsi_list = [1, 3, 6, 7, 8, 11, 12, 13, 14, 15, 39, 40, 42, 43, 52, 53, 54, 55, 67, 70, 71, 72, 88, 91, 95, 100, 108, 109, 111, 118, 122, 124, 130, 131, 135, 136, 137, 138, 143, 144, 145, 167, 168, 169, 170, 171, 173, 174, 175, 178, 179, 180, 183, 184, 185, 189, 191, 192, 202, 204, 206, 207, 208, 215, 217, 222, 223, 224, 225, 226, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 263, 264, 265, 266, 269, 275, 276, 277, 291, 296, 297, 298, 299, 300, 325, 328, 329, 330, 374, 375]
 # CC_wsi_list = [2, 41, 69, 90, 110, 123, 134, 177, 190, 201]
 
-CC_wsi_list =  [373, 376, 377, 378, 379, 380, 390, 391, 392, 400, 401, 402, 406, 407, 408, 409, 410, 422, 454, 455] #459, 460]
+# large test set
+HCC_wsi_list = [105, 117, 133, 151, 153, 154, 159, 160, 168, 169, 170, 171, 178, 180, 181, 183, 186, 189, 190, 194]
+CC_wsi_list =  [373, 376, 377, 378, 379, 380, 390, 391, 392, 400, 401, 402, 406, 407, 408, 409, 410, 422, 454, 455]
+
+# Generation Training
+# HCC_wsi_list = [104, 107, 109, 111, 120, 121, 122, 129, 131, 132, 135, 139, 141, 142, 145, 146, 149, 150, 153, 156, 159, 161, 162, 164, 165, 166, 168, 169, 171, 175]
+# HCC_wsi_list = [h+91 for h in HCC_wsi_list]
+# CC_wsi_list = [146, 158, 163, 164, 165, 315, 316, 331, 363, 459, 460, 461, 468, 469, 470, 471, 472, 473, 474, 475, 476, 483, 484, 487, 491, 492, 493, 495, 497, 499]
 
 def add_results(file_path, gen, cl, wsi, num_trial, condition, results):
     if not os.path.exists(file_path):
@@ -58,24 +85,21 @@ def add_results(file_path, gen, cl, wsi, num_trial, condition, results):
     return results
 
 def collect_results(wsi, cl, results):
-    if num_wsi == 1:
-        file_path = f"{base_path}/{wsi}/trial_{num_trial}/Metric/{wsi}_{num_class}_class_test_result.csv"
+    if error_train:
+        for error_rate in np.arange(0.01, 0.5, 0.01):
+            condition = f"ND_zscore_selected_patches_with_error_rate_{error_rate}"
+            file_path = f"{base_path}/Metric/{condition}_test_result.csv"
+            results = add_results(file_path, error_rate, cl, wsi, num_trial, "error_rate", results)
     else:
-        file_path = f"{base_path}/trial_{num_trial}/Metric/{wsi}_{num_class}_class_test_result.csv"
-    results = add_results(file_path, 0, cl, wsi, num_trial, "inference", results)
-    
-    for g in range(1, gen+1):
-        if num_wsi == 1:
-            file_path = f"{base_path}/{wsi}/trial_{num_trial}/Metric/{wsi}_Gen{g}_ND_zscore_selected_patches_by_Gen{g-1}_flip_test_result.csv"
-        else:
-            file_path = f"{base_path}/trial_{num_trial}/Metric/{wsi}_Gen{g}_ND_zscore_selected_patches_by_Gen{g-1}_flip_test_result.csv"
-        results = add_results(file_path, g, cl, wsi, num_trial, "flip", results)
-
-        if num_wsi == 1:
-            file_path = f"{base_path}/{wsi}/trial_{num_trial}/Metric/{wsi}_Gen{g}_ND_zscore_selected_patches_by_Gen{g-1}_test_result.csv"
-        else:
-            file_path = f"{base_path}/trial_{num_trial}/Metric/{wsi}_Gen{g}_ND_zscore_selected_patches_by_Gen{g-1}_test_result.csv"
-        results = add_results(file_path, g, cl, wsi, num_trial, "inference", results)
+        file_path = f"{base_path}/Metric/{wsi}_{num_class}_class_test_result.csv"
+        results = add_results(file_path, 0, cl, wsi, num_trial, "inference", results)
+        
+        for g in range(1, gen+1):
+            condition = f"Gen{g}_ND_zscore_selected_patches_by_Gen{g-1}"
+            file_path = f"{base_path}/Metric/{wsi}_{condition}_flip_test_result.csv"
+            results = add_results(file_path, g, cl, wsi, num_trial, "flip", results)
+            file_path = f"{base_path}/Metric/{wsi}_{condition}_test_result.csv"
+            results = add_results(file_path, g, cl, wsi, num_trial, "inference", results)
     
     return results
 
