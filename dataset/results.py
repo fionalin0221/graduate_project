8fe808da8d1c4e8f46cea336639ec8710451956e
fionalin0221 <fionalin0221@gmail.com>
2026-04-22 20:17:09 +0800

    add for directly compute precision, recall, f1

--- dataset/results.py
+++ dataset/results.py
@@ -2,11 +2,11 @@ import os
 import pandas as pd
 
 # Define paths
-result_type = "HCC"
+result_type = "Mix"
 num_wsi = 20
 data_num = "ALL"
-num_trial = 9
-num_class = 2
+num_trial = 6
+num_class = 4
 ep = 0
 
 # base_path = f"/workspace/Data/Results/{result_type}_NDPI/{num_wsi}WTC_Result/LP_{data_num}"
@@ -20,18 +20,19 @@ output_file = f"{base_path}/{num_wsi}WTC_LP{data_num}_trial_{num_trial}_test_res
 
 # Define trials and WSIs
 # HCC_wsi_list = []
-CC_wsi_list = []
+# CC_wsi_list = []
 
 # HCC 10WTC
 # HCC_wsi_list = [1, 6, 8, 13, 16, 27, 29, 30, 44, 45]
 # HCC_wsi_list = [1, 12, 22, 33, 45, 56, 67, 76, 89, 91]
 # HCC_wsi_list = [6, 11, 39, 52, 144]
-HCC_wsi_list = [1, 6, 8, 13, 16, 27, 29 ,30 , 31, 36, 38, 39, 44, 45, 47, 48, 49, 50, 51, 53]
+# HCC_wsi_list = [1, 6, 8, 13, 16, 27, 29, 30, 31, 36, 38, 39, 44, 45, 47, 48, 49, 50, 51, 53]
+HCC_wsi_list = [1, 6, 8, 13, 16, 31, 36, 38, 39, 44, 47, 48, 49, 50, 51, 53, 9, 19, 24, 25]
 
 # CC 10WTC
 # CC_wsi_list = [72, 108, 111, 116, 122, 124, 130, 131, 137, 138]
 # CC_wsi_list = [2, 21, 50, 69, 81]
-# CC_wsi_list = [1, 2, 3, 6, 7, 8, 11, 12, 13, 14, 15, 39, 52, 53, 54, 55, 67, 69, 70, 71]
+CC_wsi_list = [1, 2, 3, 6, 7, 8, 11, 12, 13, 14, 15, 39, 52, 53, 54, 55, 67, 69, 70, 71]
 
 # HCC 40WTC
 # HCC_wsi_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 37, 38, 39, 41, 42, 43, 44]
@@ -71,10 +72,20 @@ def add_results(file_path, cl, wsi, num_trial, results):
 
     # Read the CSV file
     df = pd.read_csv(file_path)
+    cols = ['Accuracy', f'{cl}_TP', f'{cl}_FN', f'{cl}_TN', f'{cl}_FP']
 
-    if {'Accuracy', 'F1 Score', f'{cl}_TP', f'{cl}_FN', f'{cl}_TN', f'{cl}_FP'}.issubset(df.columns):
-        row = df.iloc[0][['Accuracy', 'F1 Score', f'{cl}_TP', f'{cl}_FN', f'{cl}_TN', f'{cl}_FP']]
-        results.append([num_trial, wsi, cl] + row.tolist())  # Add trial and WSI ID for reference
+    if set(cols).issubset(df.columns):
+        acc = df.iloc[0]['Accuracy']
+        tp = df.iloc[0][f'{cl}_TP']
+        fn = df.iloc[0][f'{cl}_FN']
+        tn = df.iloc[0][f'{cl}_TN']
+        fp = df.iloc[0][f'{cl}_FP']
+
+        precision = tp / (tp + fp) if (tp + fp) > 0 else np.nan
+        recall = tp / (tp + fn) if (tp + fn) > 0 else np.nan
+        f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else np.nan
+
+        results.append([num_trial, wsi, cl, acc, tp, fn, tn, fp, precision, recall, f1])  # Add trial and WSI ID for reference
     else:
         print(f"Missing columns in {file_path}, {cl}")
 
@@ -112,7 +123,7 @@ for _wsi in CC_wsi_list:
     results = collect_results(wsi, "F", results)
             
 # --- Convert to DataFrame ---
-df = pd.DataFrame(results, columns=['Trial', 'WSI', 'Class', 'Accuracy', 'F1', 'TP', 'FN', 'TN', 'FP'])
+df = pd.DataFrame(results, columns=['Trial', 'WSI', 'Class', 'Accuracy', 'TP', 'FN', 'TN', 'FP', 'Precision', 'Recall', 'F1'])
 
 # --- Separate tumor (H/C) and normal (N) ---
 df_tumor = df[df['Class'].isin(['H', 'C'])].copy()
@@ -120,26 +131,35 @@ df_normal = df[df['Class'] == 'N'].copy()
 df_fib = df[df['Class'] == 'F'].copy()
 
 # Merge on Trial, WSI, Gen, Condition
-df_tumor = df_tumor.rename(columns={'TP': 'T_TP', 'FN': 'T_FN', 'TN': 'T_TN', 'FP': 'T_FP'})
+df_tumor = df_tumor.rename(
+    columns={'TP': 'T_TP', 'FN': 'T_FN', 'TN': 'T_TN', 'FP': 'T_FP', 'Precision': 'T_Precision', 'Recall': 'T_Recall', 'F1': 'T_F1'})
 
-merged = pd.merge(
-    df_tumor,
-    df_normal[['Trial', 'WSI', 'TP', 'FN', 'TN', 'FP']],
-    on=['Trial', 'WSI'],
-    how='left'
-)
-merged = merged.rename(columns={'TP': 'N_TP', 'FN': 'N_FN', 'TN': 'N_TN', 'FP': 'N_FP'})
+df_normal = df[df['Class'] == 'N'].copy().rename(
+    columns={'TP': 'N_TP', 'FN': 'N_FN', 'TN': 'N_TN', 'FP': 'N_FP', 'Precision': 'N_Precision', 'Recall': 'N_Recall', 'F1': 'N_F1'})
 
-merged = pd.merge(
-    merged,
-    df_fib[['Trial', 'WSI', 'TP', 'FN', 'TN', 'FP']],
-    on=['Trial', 'WSI'],
-    how='left'
-)
-merged = merged.rename(columns={'TP': 'F_TP', 'FN': 'F_FN', 'TN': 'F_TN', 'FP': 'F_FP'})
+df_fib = df[df['Class'] == 'F'].copy().rename(
+    columns={'TP': 'F_TP', 'FN': 'F_FN', 'TN': 'F_TN', 'FP': 'F_FP', 'Precision': 'F_Precision', 'Recall': 'F_Recall', 'F1': 'F_F1'})
 
-# --- Final columns (remove Class) ---
-df_output = merged[['Trial', 'WSI', 'Accuracy', 'F1', 'T_TP', 'T_FN', 'T_TN', 'T_FP', 'N_TP', 'N_FN', 'N_TN', 'N_FP', 'F_TP', 'F_FN', 'F_TN', 'F_FP']]
+merged = pd.merge(df_tumor, df_normal[['Trial', 'WSI', 'N_F1', 'N_Precision', 'N_Recall', 'N_TP', 'N_FN', 'N_TN', 'N_FP']], on=['Trial', 'WSI'], how='left')
+merged = pd.merge(merged, df_fib[['Trial', 'WSI', 'F_F1', 'F_Precision', 'F_Recall', 'F_TP', 'F_FN', 'F_TN', 'F_FP']], on=['Trial', 'WSI'], how='left')
+
+merged['Macro_F1'] = merged[['T_F1', 'N_F1', 'F_F1']].mean(axis=1)
+merged['Macro_Precision'] = merged[['T_Precision', 'N_Precision', 'F_Precision']].mean(axis=1)
+merged['Macro_Recall'] = merged[['T_Recall', 'N_Recall', 'F_Recall']].mean(axis=1)
+
+# --- Final columns ---
+custom_order = [
+    'Trial', 'WSI', 'Accuracy',
+    'T_TP', 'T_FN', 'T_TN', 'T_FP', 
+    'N_TP', 'N_FN', 'N_TN', 'N_FP', 
+    'F_TP', 'F_FN', 'F_TN', 'F_FP',
+    'T_Precision', 'N_Precision', 'F_Precision', 'Macro_Precision',
+    'T_Recall', 'N_Recall', 'F_Recall', 'Macro_Recall',
+    'T_F1', 'N_F1', 'F_F1', 'Macro_F1' 
+]
+
+existing_cols = [c for c in custom_order if c in merged.columns]
+df_output = merged[existing_cols]
 df_output.to_csv(output_file, index=False)
 
 print(f"Processed results saved to {output_file}")
